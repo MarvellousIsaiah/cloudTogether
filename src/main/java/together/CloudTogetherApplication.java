5b0df074bb957121876cb82f0ce082b51572c0c3
Marvellous <isaiahugoeze@gmail.com>
2024-11-10 22:38:35 +0100

    first commit

---
+++ src/main/java/together/CloudTogetherApplication.java
@@ -0,0 +1,13 @@
+package together;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudTogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudTogetherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/together/CloudTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package together;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudTogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
